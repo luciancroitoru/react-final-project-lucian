4da04783ed14e3deb8af5233bf58bb9a944b5f37
Lucian <lucianicroitoru@gmail.com>
2024-05-30 16:52:28 +0300

    update

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,5 +1,48 @@
-function Navbar(){
+import { useState } from 'react';
+import './Navbar.css';
 
+function Navbar() {
+    const [isHovered, setIsHovered] = useState(false);
+
+    const handleMouseEnter = () => {
+        setIsHovered(true);
+    };
+
+    const handleMouseLeave = () => {
+        setIsHovered(false);
+    };
+
+    return (
+        <div>
+            <div
+                className="menu-button"
+                onMouseEnter={handleMouseEnter}
+                onMouseLeave={handleMouseLeave}
+            >
+                ☰
+            </div>
+            <div
+                className={`drawer ${isHovered ? 'open' : ''}`}
+                onMouseEnter={handleMouseEnter}
+                onMouseLeave={handleMouseLeave}
+            >
+                <ul className="drawer__menu">
+                    <li className="drawer__item">
+                        <a href="#home" className="drawer__link">Create quote</a>
+                    </li>
+                    <li className="drawer__item">
+                        <a href="#about" className="drawer__link">Favorites</a>
+                    </li>
+                    <li className="drawer__item">
+                        <a href="#services" className="drawer__link">About me</a>
+                    </li>
+                    <li className="drawer__item">
+                        <a href="#contact" className="drawer__link">Log out</a>
+                    </li>
+                </ul>
+            </div>
+        </div>
+    );
 }
 
 export default Navbar;
--- src/components/data.js
+++ src/components/data.js
@@ -42,4 +42,43 @@ export const quotes = [
         "5/26/2024",
         false
     ),
+    new Quote(
+        "''Code even when everything seems dark in front of your eyes''",
+        "Lucian Taylor",
+        `https://picsum.photos/id/1/${resolution}`,
+        "5/21/2024",
+        false
+    ),
+
+    new Quote(
+        "''Eat, sleep, code, repeat''",
+        "Luci Taylor",
+        `https://picsum.photos/id/2/${resolution}`,
+        "5/22/2024",
+        false
+    ),
+
+    new Quote(
+        "''It is never too late to call an old friend''",
+        "Lucian Croitoru",
+        `https://picsum.photos/id/3/${resolution}`,
+        "5/23/2024",
+        false
+    ),
+
+    new Quote(
+        "''Note to myself: when code becomes pain, I become stronger''",
+        "Lucian Taylor",
+        `https://picsum.photos/id/4/${resolution}`,
+        "5/25/2024",
+        false
+    ),
+
+    new Quote(
+        "''It doesn't matter what you do, but how you do it''",
+        "Lucian Taylor",
+        `https://picsum.photos/id/5/${resolution}`,
+        "5/26/2024",
+        false
+    ),
 ]
